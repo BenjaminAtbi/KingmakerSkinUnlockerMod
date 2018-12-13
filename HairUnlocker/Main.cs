430a8b64d5cc27118cc73ea8fd174c29567dba7d
spacehamster <mail.spacehamster@gmail.com>
2018-12-13 21:34:16 +1100

    Option to change eyebrows Option to unlock incompatible options

--- HairUnlocker/Main.cs
+++ HairUnlocker/Main.cs
@@ -9,6 +9,8 @@ using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.Classes;
 using Kingmaker.ResourceLinks;
 using System.Collections.Generic;
+using Kingmaker.Blueprints.CharGen;
+using UnityEngine;
 
 namespace HairUnlocker
 {
@@ -21,14 +23,19 @@ namespace HairUnlocker
         {
             if (logger != null) logger.Log(msg);
         }
-        public static bool loaded = false;
-        public static bool enabled;
+        static bool loaded = false;
+        static bool enabled;
+        static Settings settings;
+        static Dictionary<string, CustomizationOptions> originalOptions = new Dictionary<string, CustomizationOptions>();
         static bool Load(UnityModManager.ModEntry modEntry)
         {
             try
             {
-                modEntry.OnToggle = OnToggle;
                 logger = modEntry.Logger;
+                settings = UnityModManager.ModSettings.Load<Settings>(modEntry);
+                modEntry.OnToggle = OnToggle;
+                modEntry.OnGUI = OnGUI;
+                modEntry.OnSaveGUI = OnSaveGUI;
                 SceneManager.sceneLoaded += OnSceneManagerOnSceneLoaded;
             }
             catch (Exception e)
@@ -38,19 +45,79 @@ namespace HairUnlocker
             return true;
         }
 
-        static bool OnToggle(UnityModManager.ModEntry modEntry, bool enabled)
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
         {
-            //Cannot be disabled without restart
-            return enabled;
+            enabled = value;
+            if (loaded)
+            {
+                if (!enabled) RestoreOptions();
+                else UnlockHair();
+            }
+            return true; // Permit or not.
         }
-
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            if (!loaded) return;
+            var result = GUILayout.Toggle(settings.UnlockAllHair, "Unlock All Options (Including incompatible options)");
+            if (result != settings.UnlockAllHair)
+            {
+                settings.UnlockAllHair = result;
+                RestoreOptions();
+                UnlockHair();
+            }
+#if (DEBUG)
+            DisplayInfo.ShowDoll();
+            DisplayInfo.ShowHair();
+#endif
+        }
+        static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            settings.Save(modEntry);
+        }
         static void OnSceneManagerOnSceneLoaded(Scene scene, LoadSceneMode mode)
         {
             if (scene.name == SceneName.MainMenu)
             {
                 if (loaded) return;
+                foreach(var race in Game.Instance.BlueprintRoot.Progression.CharacterRaces)
+                {
+                    if (race.AssetGuid.Length != 32) continue;
+                    originalOptions[race.name + Gender.Male] = race.MaleOptions;
+                    originalOptions[race.name + Gender.Female] = race.FemaleOptions;
+                    race.MaleOptions = new CustomizationOptions()
+                    {
+                        Heads = (EquipmentEntityLink[])race.MaleOptions.Heads.Clone(),
+                        Hair = (EquipmentEntityLink[])race.MaleOptions.Hair.Clone(),
+                        Eyebrows = (EquipmentEntityLink[])race.MaleOptions.Eyebrows.Clone(),
+                        Beards = (EquipmentEntityLink[])race.MaleOptions.Beards.Clone(),
+                    };
+                    race.FemaleOptions = new CustomizationOptions()
+                    {
+                        Heads = (EquipmentEntityLink[])race.FemaleOptions.Heads.Clone(),
+                        Hair = (EquipmentEntityLink[])race.FemaleOptions.Hair.Clone(),
+                        Eyebrows = (EquipmentEntityLink[])race.FemaleOptions.Eyebrows.Clone(),
+                        Beards = (EquipmentEntityLink[])race.FemaleOptions.Beards.Clone(),
+                    };
+                }
+                loaded = true;
+                if (!enabled) return;
                 UnlockHair();
                 
+            }
+        }
+        static void RestoreOptions()
+        {
+            foreach (var race in Game.Instance.BlueprintRoot.Progression.CharacterRaces)
+            {
+                if (race.AssetGuid.Length != 32) continue;
+                if (originalOptions.ContainsKey(race.name + Gender.Male))
+                {
+                    race.MaleOptions = originalOptions[race.name + Gender.Male];
+                }
+                if (originalOptions.ContainsKey(race.name + Gender.Male))
+                {
+                    race.FemaleOptions = originalOptions[race.name + Gender.Female];
+                }
             }
         }
         static EquipmentEntityLink[] Combine(EquipmentEntityLink[] from, EquipmentEntityLink[] to)
@@ -66,31 +133,59 @@ namespace HairUnlocker
             }
             return result.ToArray();
         }
-        static void AddHair(BlueprintRace from, BlueprintRace to)
+        /*
+         * DollState looks up the index of eyebrows by the index of heads,
+         * so existing heads are duplicated and a default eyebrow from the
+         * target class is added         * 
+         */ 
+        static void AddEyebrowsDefaultEyebrows(CustomizationOptions newSource, CustomizationOptions newTarget, CustomizationOptions originalTarget)
+        {
+            var newHeads = newTarget.Heads
+                .Where(link => originalTarget.Heads.Contains(link))
+                .Select(link => new EquipmentEntityLink() { AssetId = link.AssetId });
+            newTarget.Heads = newTarget.Heads.AddRangeToArray(newHeads.ToArray());
+            var newEyebrows = Enumerable.Repeat(newSource.Eyebrows[0], newTarget.Heads.Length - newTarget.Eyebrows.Length);
+            newTarget.Eyebrows = newTarget.Eyebrows.AddRangeToArray(newEyebrows.ToArray());
+        }
+        static void AddHair(BlueprintRace sourceRace, BlueprintRace targetRace)
         {
             foreach (var gender in new Gender[] { Gender.Male, Gender.Female })
             {
-                var fromOptions = gender == Gender.Male ? from.MaleOptions : from.FemaleOptions;
-                var toOptions = gender == Gender.Male ? to.MaleOptions : to.FemaleOptions;
-                toOptions.Hair = Combine(fromOptions.Hair, toOptions.Hair);
-                toOptions.Eyebrows = Combine(fromOptions.Eyebrows, toOptions.Eyebrows);
-                toOptions.Beards = Combine(fromOptions.Beards, toOptions.Beards);
+                var originalSource = originalOptions[sourceRace.name + gender];
+                var originalTarget = originalOptions[targetRace.name + gender];
+                var newSource = gender == Gender.Male ? sourceRace.MaleOptions : sourceRace.FemaleOptions;
+                var newTarget = gender == Gender.Male ? targetRace.MaleOptions : targetRace.FemaleOptions;
+                newTarget.Hair = Combine(newSource.Hair, newTarget.Hair);
+                newTarget.Beards = Combine(newSource.Beards, newTarget.Beards);
+                AddEyebrowsDefaultEyebrows(newSource, newTarget, originalTarget);
             }
         }
         static void UnlockHair()
         {
-            var groups = new BlueprintRace[][]
+
+            BlueprintRace[][] groups;
+            if (settings.UnlockAllHair)
             {
-                new BlueprintRace[]{
-                    ResourcesLibrary.TryGetBlueprint<BlueprintRace>("0a5d473ead98b0646b94495af250fdc4"), //Human
-                    ResourcesLibrary.TryGetBlueprint<BlueprintRace>("b7f02ba92b363064fb873963bec275ee"), //Aasimar
-                    ResourcesLibrary.TryGetBlueprint<BlueprintRace>("c4faf439f0e70bd40b5e36ee80d06be7"), //Dwarf
-                },
-                new BlueprintRace[]{
-                    ResourcesLibrary.TryGetBlueprint<BlueprintRace>("ef35a22c9a27da345a4528f0d5889157"), //Gnome
-                    ResourcesLibrary.TryGetBlueprint<BlueprintRace>("b0c3ef2729c498f47970bb50fa1acd30"), //Halfling
-                }
-            };
+                groups = new BlueprintRace[][]
+                {
+                    ResourcesLibrary.GetBlueprints<BlueprintRace>().Where(
+                        bp => bp.AssetGuid.Length == 32 && bp.AssetGuid != "f414c5b12f2296c41901e71b889ef436").ToArray()
+                };
+
+            } else {
+               groups = new BlueprintRace[][]
+                {
+                    new BlueprintRace[]{
+                        ResourcesLibrary.TryGetBlueprint<BlueprintRace>("0a5d473ead98b0646b94495af250fdc4"), //Human
+                        ResourcesLibrary.TryGetBlueprint<BlueprintRace>("b7f02ba92b363064fb873963bec275ee"), //Aasimar
+                        ResourcesLibrary.TryGetBlueprint<BlueprintRace>("c4faf439f0e70bd40b5e36ee80d06be7"), //Dwarf
+                    },
+                    new BlueprintRace[]{
+                        ResourcesLibrary.TryGetBlueprint<BlueprintRace>("ef35a22c9a27da345a4528f0d5889157"), //Gnome
+                        ResourcesLibrary.TryGetBlueprint<BlueprintRace>("b0c3ef2729c498f47970bb50fa1acd30"), //Halfling
+                    }
+                };
+            }
             foreach (var group in groups)
             {
                 foreach (var from in group)
---
+++ HairUnlocker/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityModManagerNet;
+
+namespace HairUnlocker
+{
+    public class Settings : UnityModManager.ModSettings
+    {
+        public bool UnlockAllHair = false;
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save(this, modEntry);
+        }
+    }
+}
